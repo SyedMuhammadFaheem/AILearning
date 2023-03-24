93fe91be273abdf6bd6d3406c10e9a7210416855
Syed Muhammad Faheem <syed.faheem.official@gmail.com>
2023-03-24 22:25:47 +0500

    Updated Lab 7 CSP

--- Lab 7/Task-2.py
+++ Lab 7/Task-2.py
@@ -1,8 +1,53 @@
+def alphaBetaPruning(source,depth,maximizing,alpha,beta):
+    global graph
+    if graph[source][0][1]==-1 and depth<15:
+        return graph[source][0][0]
+
+    if maximizing:
+        bestVal=-999
+        for node in graph[source]:
+            value=alphaBetaPruning(node[1],depth+1,not maximizing,alpha,beta)
+            bestVal=max(bestVal,value)
+            alpha=max(alpha,bestVal)
+            if beta<=alpha:
+                break
+        return bestVal
+    else:
+        bestVal=999
+        for node in graph[source]:
+            value=alphaBetaPruning(node[1],depth+1,not maximizing,alpha,beta)
+            bestVal=min(bestVal,value)
+            alpha=min(alpha,bestVal)
+            if beta<=alpha:
+                break
+        return bestVal
+
+
 def treeInit():
     graph=[[] for i in range(15)]
-    graph[0].append((-999,999))
-    graph[0].append((-999,999))
-    graph[1].append((-999,999))
-    graph[1].append((-999,999))
-    graph[2].append((-999,999))
-    graph[2].append((-999,999))
+    graph[0].append((0,1))
+    graph[0].append((0,2))
+    graph[1].append((0,3))
+    graph[1].append((0,4))
+    graph[2].append((0,5))
+    graph[2].append((0,6))
+    graph[3].append((0,7))
+    graph[3].append((0,8))
+    graph[4].append((0,9))
+    graph[4].append((0,10))
+    graph[5].append((0,11))
+    graph[5].append((0,12))
+    graph[6].append((0,13))
+    graph[6].append((0,14))
+    graph[7].append((2,-1))
+    graph[8].append((4,-1))
+    graph[9].append((6,-1))
+    graph[10].append((8,-1))
+    graph[11].append((1,-1))
+    graph[12].append((2,-1))
+    graph[13].append((10,-1))
+    graph[14].append((12,-1))
+    return graph
+
+graph=treeInit()
+print("The resultant value is:",alphaBetaPruning(0,0,True,-999,999))
---
+++ Lab 7/Task-4.py
@@ -0,0 +1,57 @@
+from collections import OrderedDict
+check=False
+def getChar(unique,index):
+    i=0
+    for key in unique.keys():
+        if i==index:
+            return key
+        i+=1
+
+def getVal(word,unique):
+    result=0
+    for letter in word:
+        result=(result*10)+unique[letter]
+    return result
+
+def cryptArithmetic(unique,numUsed,index,s1,s2,s3):
+    global check
+    if index==len(unique):
+        n1=getVal(s1,unique)
+        n2=getVal(s2,unique)
+        n3=getVal(s3,unique)
+        if (n1+n2)==n3:
+            print(n1,n2,n3)
+            check=True
+            unique=OrderedDict(sorted(unique.items()))
+            for keys,values in unique.items():
+                print(keys,values)
+        return
+
+    ch=getChar(unique,index)
+    for i in range(10):
+        if numUsed[i]==False:
+            unique[ch]=i
+            numUsed[i]=True
+            cryptArithmetic(unique,numUsed,index+1,s1,s2,s3)
+            if check:
+                break
+            numUsed[i]=False
+            unique[ch]=-1
+
+
+def cryptArithmeticSolve(s1,s2,s3):
+    unique=dict()
+    for letter in s1:
+        if letter not in unique.keys():
+            unique[letter]=-1
+    for letter in s2:
+        if letter not in unique.keys():
+            unique[letter]=-1
+    for letter in s3:
+        if letter not in unique.keys():
+            unique[letter]=-1
+    numUsed=[False for i in range(10)]
+    cryptArithmetic(unique,numUsed,0,s1,s2,s3)
+
+
+cryptArithmeticSolve('BASE','BALL','GAMES')
